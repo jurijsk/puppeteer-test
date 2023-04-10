fe8eea5d6015a03a861eec9026f959d0deeab88b
Jurijs Kovzels <jurijs.kovzels@outlook.com>
2023-04-10 23:49:36 +0200

    also works with puppeteer

--- screenshot/index.ts
+++ screenshot/index.ts
@@ -1,14 +1,22 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import puppeteer from "puppeteer";
 
 const httpTrigger: AzureFunction = async function(context: Context, req: HttpRequest): Promise<void> {
-	context.log("loaded crap");
-    context.res = {
-        body: "all good",
-        headers: {
-            "content-type": "text/plain"
-        }
-    }
-}
+    const url = req.query.url || "https://google.com/";
+    const browser = await puppeteer.launch();
+	const version = await browser.version();
+    const page = await browser.newPage();
+    await page.goto(url);
+    const screenshotBuffer = await page.screenshot({ fullPage: true });
+    await browser.close();
 
+    context.res = {
+        body: screenshotBuffer,
+        headers: {
+            "content-type": "image/png",
+			"chrome-version": version,
+        }
+    };
+}
 
 export default httpTrigger;
